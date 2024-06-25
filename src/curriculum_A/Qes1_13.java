8e248661e03d43c98bb077b6fd2a1d556a790954
YamamoriKohei <k.yamamori16@gmail.com>
2024-06-25 17:13:49 +0900

    途中経過

---
+++ src/curriculum_A/Qes1_13.java
@@ -0,0 +1,54 @@
+package curriculum_A;
+
+public class Qes1_13 {
+
+	public static void main(String[] args) {
+
+		        // バイト型
+
+		        byte byteVar = 10;
+		       
+		        // 短整数型
+		        short shortVar = 100;
+		        
+		        // 整数型
+		        int intVar = 1000;
+		        
+		        // 長整数型
+		        long longVar = 10000;
+		        
+		        // 単精度浮動小数点数型
+		        float floatVar = 9.5f;
+		        
+		        // 倍精度浮動小数点数型
+		        double doubleVar = 10.5;
+		        
+		        // 文字型 (char)
+		        char charVar = 'a';
+		        
+		        // 文字列型 (String)
+		        String stringVar = "ハロー";
+		        
+		        // ブーリアン型
+		        boolean booleanVar = true;
+		        
+		        // 各変数の初期値を表示して確認する
+		        System.out.println(byteVar + shortVar + intVar + longVar );
+		        System.out.println(byteVar+byteVar);
+		        System.out.println(charVar + stringVar + booleanVar );
+		        System.out.println(byteVar + shortVar + intVar + longVar + floatVar + doubleVar );
+		        System.out.println(byteVar * shortVar * intVar * longVar );
+		        System.out.println(doubleVar / shortVar);
+		        System.out.println(byteVar - shortVar );
+		        
+		      String num="20";
+		      int num1=23;
+		     System.out.println("ハローJAVA"+(num + String.valueOf(num1)));
+		    }
+	
+	
+	}
+		
+
+	
+
