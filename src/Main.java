1721d005280b90bbd1effd1a1c4810743e9a002e
YamamoriKohei <k.yamamori16@gmail.com>
2024-08-13 15:58:30 +0900

    Java_30 修正版①

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 class Main {
-  public static void main(String[] args) {
-    // 問題5: Main.javaの引数にweightの60を追加
-    Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+	public static void main(String[] args) {
+		// 問題5: Main.javaの引数にweightの60を追加
+		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
 
-    // インスタンスフィールドの値を出力
-    System.out.println(person1.name);
-    System.out.println(person1.age);
-    System.out.println(person1.height);
+		// インスタンスフィールドの値を出力
+		System.out.println(person1.name);
+		System.out.println(person1.age);
+		System.out.println(person1.height);
+		System.out.println();
 
-    // 問題8・9: person1の情報を出力
-    person1.print();
+		// 問題8・9: person1の情報を出力
+		person1.print();
 
-    // 問題10: 人数の合計を出力
-    Person.printTotalCount();
-  }
+		// 問題10: 人数の合計を出力
+		Person.printTotalCount();
+	}
 }
--- src/Person.java
+++ src/Person.java
@@ -1,36 +1,38 @@
 class Person {
-  // インスタンスフィールドを定義
-  String name;
-  int age;
-  double height;
-  double weight;  // weightフィールドを追加
+	// インスタンスフィールドを定義
+	String name;
+	int age;
+	double height;
+	double weight;
 
-  // コンストラクタを定義し、インスタンスフィールドに値をセット
-  Person(String name, int age, double height, double weight) {
-    this.name = name;
-    this.age = age;
-    this.height = height;
-    this.weight = weight;
-    count++;  // 人数をカウント
-  }
+	// コンストラクタを定義し、インスタンスフィールドに値をセット
+	Person(String name, int age, double height, double weight) {
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		count++; // 人数をカウント
+	}
 
-  // BMIを計算して返すメソッド
-  double bmi() {
-    return this.weight / (this.height * this.height);
-  }
+	// BMIを計算して返すメソッド
+	double bmi() {
+		// 小数点以下第1位を切り捨て、かつ小数点以下1桁が0となるようにする
+		return Math.floor(this.weight / (this.height * this.height));
+	}
 
-  // インスタンスの情報を出力するメソッド
-  void print() {
-    System.out.println("名前は" + this.name + "です");
-    System.out.println("年は" + this.age + "です");
-    System.out.println("BMIは" + this.bmi() + "です");
-  }
+	// インスタンスの情報を出力するメソッド
+	void print() {
+		System.out.println("名前は" + this.name + "です");
+		System.out.println("年は" + this.age + "才です");
+		System.out.println("BMIは" + this.bmi() + "です");
+		System.out.println();
+	}
 
-  // 人数をカウントするための静的変数
-  static int count = 0;
+	// 人数をカウントするための静的変数
+	static int count = 0;
 
-  // 人数の合計を出力するメソッド
-  static void printTotalCount() {
-    System.out.println("合計" + count + "人です");
-  }
+	// 人数の合計を出力するメソッド
+	static void printTotalCount() {
+		System.out.println("合計" + count + "人です");
+	}
 }
