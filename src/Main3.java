c29001d84da8abdfe5c2bd34413e060257826ea3
YamamoriKohei <k.yamamori16@gmail.com>
2024-08-13 16:09:46 +0900

    Java_32 修正版①

--- src/Main3.java
+++ src/Main3.java
@@ -1,11 +1,11 @@
 class Main3 {
-    public static void main(String[] args) {
-        Person3 person1 = new Person3("鈴木", "太郎", 20, 1.7, 60);
-        person1.print();
+	public static void main(String[] args) {
+		Person3 person1 = new Person3("鈴木", "太郎", 20, 1.7, 60);
+		person1.print();
 
-        Person3 person2 = new Person3("山田", "花子", 22, 1.5, 40);
-        person2.print();
+		Person3 person2 = new Person3("山田", "花子", 22, 1.5, 40);
+		person2.print();
 
-        Person3.printCount();
-    }
+		Person3.printCount();
+	}
 }
--- src/Person3.java
+++ src/Person3.java
@@ -1,35 +1,38 @@
 class Person3 {
-    public static int count = 0;
-    public String firstName;
-    public String lastName;  // lastNameフィールドの追加
-    public int age;
-    public double height, weight;
+	public static int count = 0;
+	public String firstName;
+	public String lastName; // lastNameフィールドの追加
+	public int age;
+	public double height, weight;
 
-    // コンストラクタのオーバーロード
-    Person3(String firstName, String lastName, int age, double height, double weight) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.height = height;
-        this.weight = weight;
-        Person3.count++;  // 人数をカウント
-    }
+	// コンストラクタのオーバーロード
+	Person3(String firstName, String lastName, int age, double height, double weight) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		Person3.count++; // 人数をカウント
+	}
 
-    public String fullName() {
-        return this.firstName + " " + this.lastName;
-    }
+	public String fullName() {
+		return this.firstName + this.lastName;
+	}
 
-    public void print() {
-        System.out.println("名前は " + this.fullName() + " です");
-        System.out.println("年は " + this.age + " です");
-        System.out.println("BMIは " + this.bmi() + " です");
-    }
+	public void print() {
+		System.out.println("名前は" + this.fullName() + "です");
+		System.out.println("年は" + this.age + "才です");
+		System.out.println("BMIは" + this.bmi() + "です");
+		System.out.println();
+	}
 
-    public double bmi() {
-        return this.weight / (this.height * this.height);
-    }
+	// BMIを計算して返すメソッド
+	double bmi() {
+		// 小数点以下第1位を切り捨て、かつ小数点以下1桁が0となるようにする
+		return Math.floor(this.weight / (this.height * this.height));
+	}
 
-    public static void printCount() {
-        System.out.println("合計 " + Person3.count + " 人です");
-    }
+	public static void printCount() {
+		System.out.println("合計 " + Person3.count + " 人です");
+	}
 }
